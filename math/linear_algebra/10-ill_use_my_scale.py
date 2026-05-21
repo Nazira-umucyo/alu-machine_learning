f1cdb6fe4b8a5720a46c1906799b5ac4ba220c1e
Nazira-umucyo <n.umucyo@alustudent.com>
2026-05-21 15:46:09 +0200

    linear algebra

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for np_shape function"""
+
+
+def np_shape(matrix):
+    """Calculate the shape of a numpy.ndarray"""
+    return matrix.shape
---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module for np_transpose function"""
+import numpy as np
+
+
+def np_transpose(matrix):
+    """Return the transpose of a matrix"""
+    return matrix.transpose()
